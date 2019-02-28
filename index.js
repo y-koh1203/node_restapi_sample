f64cc678a22c70b9a9c4e2a0e4aa532cad658ffa
Koki Yamamoto <kou.197ki@gmail.com>
2019-02-28 19:51:24 +0900

    update: jsonを返却するようにした

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public/static')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'));
+//受けとったパラメーターをもとにJSONを返却
+app.get('/api/v1/user/:id', (req, res) => {
+    console.log(req.params.id);
+    res.json({
+        id: req.params.id
+    });
 });
 
 // 無効なURLを指定された場合のルーティング
